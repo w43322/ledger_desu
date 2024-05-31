3d1cf9b59b2da8472e422d1660db361d7a06feea
Ray Wang <raywang777@foxmail.com>
2024-05-31 08:25:54 +0800

    Update clean_docker.sh

--- clean_docker.sh
+++ clean_docker.sh
@@ -2,7 +2,7 @@ sudo docker rm -f $(sudo docker ps -aq)
 sudo docker network prune
 sudo docker volume prune
 cd fixtures
-doucker-compose down -v
+docker-compose down -v
 docker volume prune
 docker container rm -f $(docker container ls -aq)
 docker-compose up -d
